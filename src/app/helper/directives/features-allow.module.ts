7ef2815564ea600be6f30a25e8bf31686095366d
Mayank <mayankgpt185@gmail.com>
2024-04-21 02:58:09 +0530

    deleted all past commied files

--- src/app/helper/directives/features-allow.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ChangeColorDirective } from './change-color.directive';
-import { FeatureAllowDirective } from './features-allow.directive';
-
-@NgModule({
-    declarations: [ChangeColorDirective],
-    exports: [ChangeColorDirective]
-  })
-  export class FeatureAllowModule {}
